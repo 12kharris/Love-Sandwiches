fd260eadf91d1bf6477f6e9b6b1345a4579313dc
12kharris Harris <kurtharris1541@gmail.com>
2024-02-06 19:41:58 +0000

    Add function to update sales google sheet

--- run.py
+++ run.py
@@ -28,7 +28,7 @@ def get_sales_data():
         sales_data = data_str.split(",")
         validated = validate_data(sales_data)
 
-    print("Data received")
+    print("Data received\n")
     return sales_data
 
 
@@ -50,5 +50,16 @@ def validate_data(values):
 
     return True
 
+def update_sales_worksheet(data):
+    """
+    Update sales worksheet, add new row with the list data provided
+    """
+    print("Updating sales worksheet...\n")
+    sales_worksheet = SHEET.worksheet("sales")
+    sales_worksheet.append_row(data)
+    print("Sales worksheet updated successfully\n")
+
 
 data = get_sales_data()
+sales_data = [int(value) for value in data]
+update_sales_worksheet(sales_data)
